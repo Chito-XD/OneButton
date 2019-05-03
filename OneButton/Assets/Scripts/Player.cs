17705deea841e360deb1cc1464b01e9cc4dc6d51
chito0399 <A01338798@itesm.mx>
2019-05-03 14:08:58 -0500

    Timer

---
+++ OneButton/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private bool direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction)
+        {
+            gameObject.transform.Translate(new Vector3(speed, 0, 0));
+        }   
+        else
+        {
+            gameObject.transform.Translate(new Vector3(-speed, 0, 0));
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction = !direction;
+        }
+    }
+}
